e591fbd12f19621e5685b3d9a8214cefb37b8b66
Mathura <mathura.santhalingam@gmail.com>
2026-03-19 22:54:05 +0100

    top5 + eviter les noms propres pour phobie

--- DWL_TAL/Projet_TAL_json.py
+++ DWL_TAL/Projet_TAL_json.py
@@ -5,22 +5,15 @@ import numpy as np
 from nltk.corpus import stopwords
 from nltk import RegexpTokenizer
 from nltk.stem import WordNetLemmatizer
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-#------------A VERIFIER---------------------
-# Voici comment on stocke dans le film_csv : 
-# { 
-#     5816 : { 
-#         "title" : "Harry Potter and the chamber of secret",
-#         "scenes" : wide helicopter shot. privet drive. camera cranes down, down, over the rooftops 
-#             harry pages through a scrapbook, stops on a moving photo...
-#     }
-# }
-# 
+# ******************************************************************************** #
+# ************************ Étape 0 : Nettoyage préalable ************************* #
+# ******************************************************************************** #
 
-
+# Nettoyage du script pour n'avoir que l'essentiel
 def parsing_script(script):
+    # Extraction uniquement du texte compris entre les balises <scene_description>
     script = script.replace("<", " <").replace(">","> ")
     script = script.split()
 
@@ -37,60 +30,84 @@ def parsing_script(script):
             continue
 
         if balise_scene : 
-            scene_description.append(mot.lower()) 
+            scene_description.append(mot) 
     return " ".join(scene_description)
 
+# Chargement des données
+temps_total = time.time()
 df = pd.read_json("Dataset_Projet_TAL/collection_test/corpus.json", encoding="utf-8")
 df['text'] = df['text'].apply(parsing_script)
 
 docs = df.set_index('doc_id').to_dict(orient='index')
+
+# ******************************************************************************** #
+# ******************** Étape 1 : Création de l'index inversé ********************* #
+# ******************************************************************************** #
 
 start_posting = time.time()
 postings = {}
 stopwords = set(stopwords.words("english"))
 tokenizer = RegexpTokenizer(r"\w+")
 lemmatizer = WordNetLemmatizer()
+# Pour optimiser en évitant de re-lemmatiser plusieurs fois un même mot
+lemme = {}
+
 for filmID, data in docs.items():
     script = data["text"]
     tokens=tokenizer.tokenize(script)
-    for t in tokens : 
-        if t not in stopwords:
-            t = lemmatizer.lemmatize(t)
+    for token in tokens :
+
+        # Lemmatisation
+        if token not in stopwords:
+            if token not in lemme : 
+                lemme[token] = lemmatizer.lemmatize(token)
+            t = lemme[token]
+
+            # Remplissage de l'index inversé
             if t not in postings :
                 postings[t] = {}
             if filmID not in postings[t]:
                 postings[t][filmID] = 1
             else : 
                 postings[t][filmID] +=1
-
 end_posting = time.time()
 
 
+# ******************************************************************************** #
+# ********************** Étape 2 : Calcul des scores TF-IDF ********************** #
+# ******************************************************************************** #
 
 start = time.time()
 N = len(docs)
-#Optimisation possible : 
-#   Au lieu de passer de panda à dico, on reste sur pandas
-#   Et donc on pourra utiliser TfidfVectorizer() sur une colonne où on aura enlever les stopwords , fait les lemmatisation 
 
 tf_idf = {}
+
+# Calcul de l'IDF pour chaque terme du vocabulaire 
 idf_score = {terme : math.log(N / len(postings[terme])) for terme in postings}
+# Création d'une liste des mots, elle doit être fixe pour comparer les vecteurs
 liste_terme = list(postings.keys())
-for filmID in docs : 
+
+# Initialisation des vecteurs TF*IDF
+for filmID in docs :
     tf_idf[filmID] = [0.0] * len(liste_terme)
 
+# Remplissage des vecteurs TF*IDF
 for i,terme in enumerate(postings):
     score_idf = idf_score[terme]
     for filmID,tf in postings[terme].items():
         if filmID in tf_idf  : 
             tf_idf[filmID][i] = tf * score_idf
-
 end = time.time()
 
-query = "I am afraid of spider"
+# ******************************************************************************** #
+# ********************** Étape 3 : Traitement de la requête ********************** #
+# ******************************************************************************** #
+
+query = input("Enter your query : ")
 tokens = tokenizer.tokenize(query.lower())
 queryterms = [lemmatizer.lemmatize(t) for t in tokens if t not in stopwords]
 
+# Création du vecteur de la requête
 vecteur_query = [0.0] * len(liste_terme)
 for mot in queryterms : 
     if mot in postings : 
@@ -98,18 +115,29 @@ for mot in queryterms :
         tf = queryterms.count(mot)
         vecteur_query[index] = tf * idf_score[mot]
 
+# ******************************************************************************** #
+# ****************** Étape 4 : Calcul des scores de similarité ******************* #
+# ******************************************************************************** #
+        
 vect_query_arr = np.array(vecteur_query).reshape(1,-1)
-maximum = -2
-not_watch = None
-for filmID, vecteur in tf_idf.items() : 
+
+score_similarite = {}
+for filmID, vecteur in tf_idf.items() :
     vecteur_film_arr = np.array(vecteur).reshape(1,-1)
+    # Similarité cosinus
     cos = cosine_similarity(vect_query_arr,vecteur_film_arr)[0][0]
-    if(cos > maximum):
-        maximum = cos
-        not_watch = filmID 
+    score_similarite[filmID] = cos
 
-print("LE FILM A EVITER : ", docs[not_watch]["title"])
-print("SCORE DE SIMILARITE :", maximum)
+# Tri pour avoir les 5 meilleurs scores -> les films à éviter à toçut prix
+pires_5_films = sorted(score_similarite.items(), key=lambda x: x[1], reverse = True)[:5]
 
-print(f"Execution temps tf-idf: {end - start}")
-print(f"Execution temps postings: {end_posting - start_posting}")
+print("Vous devriez éviter le(s) film(s) : ")
+for rang, (filmID, score) in enumerate(pires_5_films, 1):
+    titre = docs[filmID]["title"]
+    print(f"{rang}. {titre} (Score de similarité : {score})")
+
+# Temps d'exécution
+print(f"Temps d'exécution du tf-idf: {end - start}")
+print(f"Temps d'exécution des postings: {end_posting - start_posting}")
+temps_total_end = time.time()
+print(f"Temps d'exécution total du programme : {temps_total_end - temps_total}")
